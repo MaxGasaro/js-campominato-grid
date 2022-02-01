0fbf605f09565929b258a8bd28fd0bdd6cb04a1f
Massimiliano Gasaro <massimilianogasaro95@gmail.com>
2022-02-01 18:05:51 +0100

    first milestone

---
+++ main.js
@@ -0,0 +1,117 @@
+document.getElementById('start-play').addEventListener('click', play)
+
+//funzione per settare il gioco
+function play() {
+
+    const difficolta = document.getElementById('difficoltà').value;
+
+    const NUMERO_BOMBE = 16;
+
+    const gioco = document.getElementById('gioco');
+    //resetto campo di gioco
+    gioco.innerHTML = '';
+
+    let numeroCelle;
+    let cellePerRiga;
+    const tentativi = [];
+
+    switch (difficolta) {
+        case "facile":
+        //con default facciamo in modo che anche se si cerca di bypassare 
+        //una delle tre possibilita si finisce dentro questo caso
+        //avrei potuto metterlo anche in altri casi o in fondo    
+        default:
+            numeroCelle = 100;
+            break;
+        case "intermedio":
+            numeroCelle = 81;
+            break;
+        case "difficile":
+            numeroCelle = 49;
+            break;
+    }
+
+    generaCampoGioco(numeroCelle);
+    const bombe = generaBombe(NUMERO_BOMBE, numeroCelle);
+
+    function generaCampoGioco(numeroCelle) {
+        //math.sqrt serve per fornire la radice quadrata
+        cellePerRiga = Math.sqrt(numeroCelle);
+        
+        for (let i = 1; i <= numeroCelle; i++) {
+            const nodo = document.createElement('div');
+            nodo.classList.add('box');
+
+            const dimensione = `calc(100% / ${cellePerRiga})`;
+            nodo.style.width = dimensione;
+            nodo.style.height = dimensione;
+
+            nodo.textContent = i;
+        
+            nodo.addEventListener('click', handleCellClick);
+        
+            gioco.appendChild(nodo);
+        
+        }
+        
+        return true;
+    }
+
+    function handleCellClick() {
+        this.classList.add('clicked');
+        //alert(this.innerText);
+        this.removeEventListener('click', handleCellClick);
+
+        const cell = parseInt(this.innerText);
+
+        if (bombe.includes(cell)) {
+
+            alert('sono su una bomba,fine del gioco');
+
+        } else {
+
+            tentativi.push(cell);
+
+        }
+
+        console.log('al momento hai cliccato su: ' + tentativi);
+    }
+
+    function terminaGioco() {
+        const quadrati = document.getElementsByClassName('box');
+
+        for (let i=0; i<quadrati.length; i++) {
+            if(bombe.includes(parseInt(quadrati[i].innerText))) {
+                quadrati[i].classList.add('bomb');
+            }
+            // qua dovrei rimuovere ascoltare di eventi su quadrati[i]
+            //e stampo la lunghezza dei tentativi
+        }
+    }
+
+    function generaBombe(numeroBombe, numeroCelle) {
+
+        const bombeGenerate = [];
+
+        while (bombeGenerate.length < numeroBombe) {
+
+            const bomba = getRandomNumber(1, numeroCelle);
+
+            if (!bombeGenerate.includes(bomba)) {
+                
+                bombeGenerate.push(bomba);
+
+            }
+
+        }
+
+        return bombeGenerate;
+
+    }
+
+}
+
+function getRandomNumber(min,max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
